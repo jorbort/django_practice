4c96bd8c0f6c564e15251eb2ac0d24c51e74e041
Jorge Bortolotti Suarez <jbortolo@car13s3.42barcelona.com>
2024-08-22 15:31:48 +0200

    el docker de django funcionando mas o menos bien aunque falta integrarle postgres y el poder configurarlo con requierements.txt

--- backend/django_start.sh
+++ backend/django_start.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-mkdir django_project && cd django_project\
-&& python3 -m venv .venv && source .venv/bin/activate\
-&& pip install django 
 
-python manage.py runserver 0.0.0.0:8000
+python3 -m venv .venv && source .venv/bin/activate\
+&& pip install django && pip install psycopg2-binary\
+
+ls -la 
+cd trascendance
+ls -la
+
+python3 manage.py runserver 0.0.0.0:8000

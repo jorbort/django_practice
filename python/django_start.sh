0a772bd66e1e2f0014a31fa0a0dc37bc329bda51
Jorge Bortolotti Suarez <jbortolo@car13s3.42barcelona.com>
2024-08-22 12:52:28 +0200

    restructuradas algunas cosas para que sea mas parecido a la estructura real, con un docker compose para conectar y un requirements para pip

--- python/django_start.sh
+++ python/django_start.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-cd /home && mkdir django_project && cd django_project\
+mkdir django_project && cd django_project\
 && python3 -m venv .venv && source .venv/bin/activate\
-&& pip install --upgrade pip && pip install psycopg2-binary\
-&& pip install django && django-admin startproject mysite .
+&& pip install django 
 
 python manage.py runserver 0.0.0.0:8000
